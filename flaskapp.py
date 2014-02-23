3f9ec92c7ef9e4e966413e3b6b4a41e182bc7208
Duncan cook <duncan.cook@theletterd.co.uk>
2014-02-22 21:21:09 -0800

    style and sockets

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import jsonify
+from flask import json
 from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
+
+from flask_sockets import Sockets
 
 import util.sass
 import util.usb_resetter
@@ -13,6 +16,7 @@ import settings
 import os
 
 app = Flask(__name__)
+sockets = Sockets(app)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -38,6 +42,26 @@ def take_pics():
 
     return jsonify(success=True)
 
+@sockets.route('/ws_take_pics')
+def ws_take_pics(ws):
+    message = json.loads(ws.receive())
+    num_pics = int(message['num_pics'])
+
+    assert num_pics in settings.framenum_values
+
+    try:
+        ws.send(json.dumps({"message":"Resetting usb"}))
+        util.usb_resetter.reset_usb(settings.manufacturer)
+
+        ws.send(json.dumps({"message":"Taking %d pics" % num_pics}))
+        util.photo.take_photos(num_pics)
+
+        ws.send(json.dumps({"message":"Making thumbnails"}))
+        util.photo.make_thumbnails(num_pics)
+    except:
+        ws.send(json.dumps({"message":":("}))
+
+    ws.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- static/js/remote.js
+++ static/js/remote.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('.action-button').click(function(event) {
+/*    $('.action-button').click(function(event) {
 	var num_frames = $(this).attr('value');
 
 	$.ajax({
@@ -10,5 +10,29 @@ $(document).ready(function() {
 	    success: location.reload(),
 	    async: false
 	});
+    });
+*/
+    $('.action-button').click(function(event) {
+	// maybe it makes sense to disable all the buttons here.
+	$('.action-button').addClass('disabled');
+
+	var num_pics = $(this).attr('value');
+
+	var ws = new WebSocket("ws://" + document.location.host + "/ws_take_pics");
+
+	ws.onopen = function() {
+	    ws.send(JSON.stringify({num_pics: num_pics}));
+	};
+
+	ws.onmessage = function(msgevent) {
+	    var message = $.parseJSON(msgevent.data);
+	    $('#message-area').text(message.message);
+	};
+
+	ws.onclose = function(msgevent) {
+	    $('.action-button').removeClass('disabled');
+	};
+
+	// an then re-enable here, or on on-close.
     });
 });
